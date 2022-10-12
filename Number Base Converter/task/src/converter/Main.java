e651685f8e61dec0021bced643dda9539751c7cb
DenisPeleng <rangerby@icloud.com>
2022-10-12 21:03:36 +0400

    Completed project

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -3,56 +3,52 @@ package converter;
 import java.util.Scanner;
 
 public class Main {
-    static boolean isRunning = true;
+    static boolean isRunningProgram = true;
+    static boolean isRunningSecondMenu = false;
+    static int currentBaseSource = 0;
+    static int currentBaseTarget = 0;
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        while (isRunning) {
+        while (isRunningProgram) {
             showMenu();
         }
-
-
     }
 
     static void showMenu() {
-        System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
-        String command = scanner.next();
-        switch (command) {
-            case "/from" -> showConvertFromDecMenu();
-            case "/to" -> showConvertToDecMenu();
-            case "/exit" -> isRunning = false;
+        System.out.println("Enter two numbers in format: {source base} {target base} (To quit type /exit)");
+        String command = scanner.nextLine();
+
+        if ("/exit".equals(command)) {
+            isRunningProgram = false;
+        } else {
+            isRunningSecondMenu = true;
+            while (isRunningSecondMenu) {
+                showSecondLevelMenu(command);
+            }
         }
     }
 
-    private static void showConvertToDecMenu() {
-        System.out.println("Enter source number:");
-        String numberInVarBase = scanner.next();
-        System.out.println("Enter source base: ");
-        int base = scanner.nextInt();
-        int conversionResult = 0;
-        switch (base) {
-            case 2 -> conversionResult = NumberConvertors.fromBinToDec(numberInVarBase);
-            case 8 -> conversionResult = NumberConvertors.fromOctToDec(numberInVarBase);
-            case 16 -> conversionResult = NumberConvertors.fromHexToDec(numberInVarBase);
-            default -> System.out.println("Wrong base entered");
-        }
-        System.out.println("Conversion to decimal result: " + conversionResult);
-        System.out.println();
-    }
+    private static void showSecondLevelMenu(String command) {
+        String[] bases = command.split(" ");
+        currentBaseSource = Integer.parseInt(bases[0]);
+        currentBaseTarget = Integer.parseInt(bases[1]);
+        System.out.printf("Enter number in base %d to convert to base %d (To go back type /back)%n", currentBaseSource, currentBaseTarget);
+        String newCommand = scanner.nextLine();
+        if ("/back".equals(newCommand)) {
+            currentBaseSource = 0;
+            currentBaseTarget = 0;
+            isRunningSecondMenu = false;
+        } else {
+            String resultNumber;
+            if (newCommand.contains(".")) {
+                resultNumber = NumberConvertors.universalFractionalConverter(newCommand, currentBaseSource, currentBaseTarget);
 
-    private static void showConvertFromDecMenu() {
-        System.out.println("Enter number in decimal system:");
-        int numberInDec = scanner.nextInt();
-        System.out.println("Enter target base: ");
-        int base = scanner.nextInt();
-        String conversionResult = "";
-        switch (base) {
-            case 2 -> conversionResult = NumberConvertors.numberToBin(numberInDec);
-            case 8 -> conversionResult = NumberConvertors.numberToOctal(numberInDec);
-            case 16 -> conversionResult = NumberConvertors.numberToHex(numberInDec);
-            default -> System.out.println("Wrong base entered");
+            } else {
+                resultNumber = NumberConvertors.universalIntegerConverter(newCommand, currentBaseSource, currentBaseTarget);
+
+            }
+            System.out.printf("Conversion result: %s%n%n", resultNumber);
         }
-        System.out.println("Conversion result: " + conversionResult);
-        System.out.println();
     }
 }
--- Number Base Converter/task/src/converter/NumberConvertors.java
+++ Number Base Converter/task/src/converter/NumberConvertors.java
@@ -1,28 +1,111 @@
 package converter;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
+
 public class NumberConvertors {
+    private static final String[] baseSymbols = new String[36];
 
-    public static String numberToHex(int numberInDec) {
-        return Integer.toHexString(numberInDec);
+    private static void fillBaseSymbols() {
+
+        for (int i = 0; i < 36; i++) {
+            if (i > 9) {
+                for (char j = 'a'; j <= 'z'; j++) {
+                    baseSymbols[i] = String.valueOf(j);
+                    i++;
+                }
+            } else {
+                baseSymbols[i] = String.valueOf(i);
+            }
+        }
+
     }
 
-    public static String numberToOctal(int numberInDec) {
-        return Integer.toOctalString(numberInDec);
+    public static String universalIntegerConverter(String number, int sourceBase, int targetBase) {
+        fillBaseSymbols();
+        BigInteger bigIntegerDec = numberIntegerToDec(number, sourceBase);
+        return numberIntegerToTarget(bigIntegerDec, targetBase);
     }
 
-    public static String numberToBin(int numberInDec) {
-        return Integer.toBinaryString(numberInDec);
+    private static String numberIntegerToTarget(BigInteger bigIntegerDec, int targetBase) {
+        StringBuilder result = new StringBuilder();
+        BigInteger bigIntegerDivide = bigIntegerDec;
+        while (bigIntegerDivide.compareTo(BigInteger.valueOf(0)) >= 1) {
+            BigInteger[] bigIntegers = bigIntegerDivide.divideAndRemainder(BigInteger.valueOf(targetBase));
+            int indexDigitTargetBase = Integer.parseInt(bigIntegers[1].toString());
+            result.append(baseSymbols[indexDigitTargetBase]);
+            bigIntegerDivide = bigIntegers[0];
+        }
+        String resultStr = result.reverse().toString();
+        resultStr = resultStr.isEmpty() ? "0" : resultStr;
+        return resultStr;
     }
 
-    public static int fromBinToDec(String numberInBin) {
-        return Integer.parseInt(numberInBin, 2);
+    private static int indexOfElement(String value) {
+        for (int i = 0; i < NumberConvertors.baseSymbols.length; i++) {
+            if (NumberConvertors.baseSymbols[i].equals(value.toLowerCase())) {
+                return i;
+            }
+        }
+        return -1;
     }
 
-    public static int fromOctToDec(String numberInOct) {
-        return Integer.parseInt(numberInOct, 8);
+    private static BigInteger numberIntegerToDec(String number, int sourceBase) {
+        String[] arrayWithDigits = number.split("");
+        BigInteger bigIntegerResult = BigInteger.ZERO;
+        BigInteger bigIntegerBase = new BigInteger(String.valueOf(sourceBase));
+
+        for (int i = 0; i < arrayWithDigits.length; i++) {
+            BigInteger fromDecTable = new BigInteger(String.valueOf(indexOfElement(arrayWithDigits[arrayWithDigits.length - 1 - i])));
+            BigInteger secondNumber = bigIntegerBase.pow(i).multiply(fromDecTable);
+            bigIntegerResult = bigIntegerResult.add(secondNumber);
+        }
+
+        return bigIntegerResult;
     }
 
-    public static int fromHexToDec(String numberInHex) {
-        return Integer.parseInt(numberInHex, 16);
+    public static String universalFractionalConverter(String number, int sourceBase, int targetBase) {
+        fillBaseSymbols();
+        String[] numberInTwoParts = number.split("\\.");
+        BigInteger bigIntegerDec = numberIntegerToDec(numberInTwoParts[0], sourceBase);
+        String resultStr = numberIntegerToTarget(bigIntegerDec, targetBase);
+        BigDecimal bigFractionalDec = numberFractionalToDec(numberInTwoParts[1], sourceBase);
+        String resultStr2 = numberFractionalToTarget(bigFractionalDec, targetBase);
+
+
+        return resultStr + "." + resultStr2;
+    }
+
+
+    private static BigDecimal numberFractionalToDec(String number, int sourceBase) {
+        String[] arrayWithDigits = number.split("");
+        BigDecimal bigDecimalResult = BigDecimal.ZERO;
+        BigDecimal bigDecimalBase = new BigDecimal(String.valueOf(sourceBase));
+        for (int i = 0; i < arrayWithDigits.length; i++) {
+            BigDecimal fromDecTable = new BigDecimal(String.valueOf(indexOfElement(arrayWithDigits[i])));
+            BigDecimal secondNumber = new BigDecimal(1).divide(bigDecimalBase.pow(i + 1), 10, RoundingMode.CEILING).multiply(fromDecTable);
+            bigDecimalResult = bigDecimalResult.add(secondNumber);
+        }
+        return bigDecimalResult;
+    }
+
+    private static String numberFractionalToTarget(BigDecimal bigFractionalDec, int targetBase) {
+        StringBuilder result = new StringBuilder();
+        BigDecimal bigFractionalMulti = bigFractionalDec;
+        while (bigFractionalMulti.compareTo(BigDecimal.valueOf(0)) >= 1 && result.length() < 5) {
+            bigFractionalMulti = bigFractionalMulti.multiply(BigDecimal.valueOf(targetBase));
+            BigDecimal integerPart = bigFractionalMulti.setScale(0, RoundingMode.DOWN);
+            BigDecimal fractionalPart = bigFractionalMulti.subtract(integerPart);
+            int indexDigitTargetBase = Integer.parseInt(integerPart.toString());
+            result.append(baseSymbols[indexDigitTargetBase]);
+            bigFractionalMulti = fractionalPart;
+        }
+        if (result.length() < 5) {
+            result.append("0".repeat(Math.max(0, 5 - result.length())));
+        }
+
+        return result.toString();
     }
 }
